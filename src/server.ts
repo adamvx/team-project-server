95b2609bb6c4fbcd6a6a9eb0db65fa61315a5ccd
Daniel Oros <oros.dano@gmail.com>
2021-11-13 16:53:40 +0100

    added support for multiple rooms

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,54 @@
-import WebSocket, { WebSocketServer } from "ws";
+import express from 'express';
+import { createServer } from 'http';
+import { parse } from 'url';
+import WebSocket, { WebSocketServer } from 'ws';
+import crypto from 'crypto'
 
-//Web socket server, basic
-const wss = new WebSocketServer({
-  port: 5677
-});
+const app = express();
+const server = createServer(app);
 
-wss.on('connection', (ws) => {
-  console.log('New client connected!');
+const rooms: { key: string, socket: WebSocketServer }[] = [];
 
-  ws.on('message', (data) => onClinetMessage(ws, data));
+app.get('/create', (req, res) => {
 
-  ws.on('close', () => {
-    console.log('Client has disconnected!');
+  const id = crypto.randomBytes(12).toString('hex');
+  const wss = new WebSocketServer({ noServer: true });
+
+  wss.on('connection', (ws) => {
+    console.log('New client connected!');
+
+    ws.on('message', (data) => onClinetMessage(wss, ws, data));
+
+    ws.on('close', () => {
+      console.log('Client has disconnected!');
+    });
+  });
+
+  rooms.push({ key: id, socket: wss });
+  res.send(id);
+
+})
+
+server.on('upgrade', function upgrade(request, socket, head) {
+  const { pathname } = parse(request.url ?? '');
+
+  const room = rooms.find((room) => {
+    if (room.key === pathname) {
+      return room;
+    }
+  })
+
+  if (room === undefined) {
+    socket.destroy();
+    return;
+  }
+
+  room.socket.handleUpgrade(request, socket as any, head, function done(ws) {
+    room.socket.emit('connection', ws, request);
   });
 });
 
-const onClinetMessage = (ws: WebSocket, data: WebSocket.RawData) => {
+const onClinetMessage = (wss: WebSocketServer, ws: WebSocket, data: WebSocket.RawData) => {
   wss.clients.forEach((client) => {
     if (client !== ws && client.readyState === WebSocket.OPEN) {
       client.send(data, { binary: true });
@@ -23,3 +56,4 @@ const onClinetMessage = (ws: WebSocket, data: WebSocket.RawData) => {
   });
 }
 
+server.listen(5677);
