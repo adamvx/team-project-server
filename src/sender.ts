340cace8873591f25720e9d07ae7242d4fccc53b
Adam Vician <adamvician@gmail.com>
2022-04-02 21:42:12 +0200

    Added dev room

--- src/sender.ts
+++ src/sender.ts
@@ -1,27 +1,22 @@
-import axios from 'axios';
-import { readFileSync } from 'fs';
-import { WebSocket } from 'ws';
+import { readFileSync } from "fs";
+import { WebSocket } from "ws";
 
 (async () => {
-  const res = await axios.get('http://161.35.216.12:5677/create')
-  const id = res.data.id
-  console.log("Room id:", id)
+	const id = "0000-0000-0000";
+	console.log("Room id:", id);
 
-  const ws = new WebSocket(`ws://161.35.216.12:5677/${id}`);
-  let i = 0;
+	const ws = new WebSocket(`ws://161.35.216.12:5677/${id}`);
+	let i = 0;
 
-  ws.on("open", () => {
-    console.log("Sender has connected and sending data");
-    setInterval(() => {
-      const dracoData = readFileSync(`./drc/file${i}.drc`);
-      ws.send(dracoData);
-      i += 1;
-      if (i > 120) {
-        i = 0;
-      }
-    }, 41);
-  });
-
-})()
-
-
+	ws.on("open", () => {
+		console.log("Sender has connected and sending data");
+		setInterval(() => {
+			const dracoData = readFileSync(`./drc/file${i}.drc`);
+			ws.send(dracoData);
+			i += 1;
+			if (i > 120) {
+				i = 0;
+			}
+		}, 41);
+	});
+})();
--- src/server.ts
+++ src/server.ts
@@ -1,64 +1,69 @@
-import express from 'express';
-import { createServer } from 'http';
-import WebSocket, { WebSocketServer } from 'ws';
-import { generateId } from './utils';
+import express from "express";
+import { createServer } from "http";
+import WebSocket, { WebSocketServer } from "ws";
+import { generateId } from "./utils";
 
 const app = express();
 const server = createServer(app);
 
-let rooms: { key: string, socket: WebSocketServer }[] = [];
+let rooms: { key: string; socket: WebSocketServer }[] = [
+	{ key: "0000-0000-0000", socket: new WebSocketServer({ noServer: true }) },
+];
 
-app.get('/create', (req, res) => {
+app.get("/create", (req, res) => {
+	const id = generateId();
+	const wss = new WebSocketServer({ noServer: true });
 
-  const id = generateId();
-  const wss = new WebSocketServer({ noServer: true });
+	wss.on("connection", (ws) => {
+		console.log(`New client connected on server with id: ${id}!`);
 
-  wss.on('connection', (ws) => {
-    console.log(`New client connected on server with id: ${id}!`);
+		ws.on("message", (data) => onClinetMessage(wss, ws, data));
 
-    ws.on('message', (data) => onClinetMessage(wss, ws, data));
+		ws.on("close", () => {
+			console.log(`Client has disconnected on server with id: ${id}!`);
+			if (wss.clients.size === 0) {
+				console.log(
+					`All client has disconnected terminating server with id: ${id}!`
+				);
+				wss.close();
+				// rooms = rooms.filter(x => x.key !== id)
+			}
+		});
+	});
 
-    ws.on('close', () => {
-      console.log(`Client has disconnected on server with id: ${id}!`);
-      if (wss.clients.size === 0) {
-        console.log(`All client has disconnected terminating server with id: ${id}!`);
-        wss.close()
-        rooms = rooms.filter(x => x.key !== id)
-      }
-    });
-  });
-
-  rooms.push({ key: id, socket: wss });
-  res.json({ id });
-
-})
-
-server.on('upgrade', function upgrade(request, socket, head) {
-
-  const room = rooms.find((room) => {
-    if (room.key === request.url?.replace('/', '')) {
-      return room;
-    }
-  })
-
-  if (room === undefined) {
-    socket.destroy();
-    return;
-  }
-
-  room.socket.handleUpgrade(request, socket as any, head, function done(ws) {
-    room.socket.emit('connection', ws, request);
-  });
+	rooms.push({ key: id, socket: wss });
+	res.json({ id });
 });
 
-const onClinetMessage = (wss: WebSocketServer, ws: WebSocket, data: WebSocket.RawData) => {
-  wss.clients.forEach((client) => {
-    if (client !== ws && client.readyState === WebSocket.OPEN) {
-      client.send(data, { binary: true });
-    }
-  });
-}
+server.on("upgrade", function upgrade(request, socket, head) {
+	const room = rooms.find((room) => {
+		if (room.key === request.url?.replace("/", "")) {
+			return room;
+		}
+	});
+
+	if (room === undefined) {
+		socket.destroy();
+		return;
+	}
+
+	room.socket.handleUpgrade(request, socket as any, head, function done(ws) {
+		room.socket.emit("connection", ws, request);
+	});
+});
+
+const onClinetMessage = (
+	wss: WebSocketServer,
+	ws: WebSocket,
+	data: WebSocket.RawData
+) => {
+	wss.clients.forEach((client) => {
+		if (client !== ws && client.readyState === WebSocket.OPEN) {
+			client.send(data, { binary: true });
+		}
+	});
+};
 
 server.listen(5677, () => {
-  console.log('🚀 Server is running!')
+	console.log("🚀 Server is running!");
 });
